ec03ba7abb0b14fcf5ae7214f4a6eb361d8d832d
AntCypher <theantcypher@gmail.com>
2021-12-13 15:38:04 +0530

    add Porson Component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>My First App</h1>
-    //   </div>
-    // );
+    return (
+      <div className="App">
+        <h1>My First App</h1>
+        <Person />
+      </div>
+    );
 
-    return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'));
+    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'));
 
   }
 }
